27c33e9ea30892efaf671cc29603bf9488b75bf8
ZioDude <akyriakouu@gmail.com>
2025-06-16 15:29:25 +0300

    Add complete pricing section with 4-tier structure and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/sections/HeroSection";
 import PortfolioSection from "@/components/sections/PortfolioSection";
+import PricingSection from "@/components/sections/PricingSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <PortfolioSection />
+      <PricingSection />
       {/* We can add more sections here later */}
     </>
   );
